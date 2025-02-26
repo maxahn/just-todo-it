ca67d93951203f5eead938d70bb71bf679113ee5
Max Ahn <ahnmaxwell@gmail.com>
2025-02-26 10:37:02 -0700

    - prepped for build and update

--- app/tabs/(tabs)/player.tsx
+++ app/tabs/(tabs)/player.tsx
@@ -3,14 +3,16 @@ import { Center } from "@/components/ui/center";
 import { Divider } from "@/components/ui/divider";
 import { Heading } from "@/components/ui/heading";
 import { Text } from "@/components/ui/text";
+import { VStack } from "@/components/ui/vstack";
 
 export default function Player() {
   return (
-    <Center className="flex-1">
-      <Heading className="font-bold text-2xl">Player</Heading>
-      <Divider className="my-[30px] w-[80%]" />
-      <Text className="p-4">Example below to use gluestack-ui components.</Text>
-      <EditScreenInfo path="app/(app)/(tabs)/tab2.tsx" />
+    <Center className="flex-1 p-8">
+      <VStack>
+        <Text className="text-xl font-bold">
+          TODOIST KEY: {process.env.EXPO_PUBLIC_TODOIST_API_KEY}
+        </Text>
+      </VStack>
     </Center>
   );
 }
